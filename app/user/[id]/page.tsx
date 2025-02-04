f29f71d8945880ba53db621004e0dfcc6adc6349
Chingis007 <karaulnekerell@gmail.com>
2025-02-04 15:58:50 +0200

    dasda

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { auth } from "../../../auth"
-import { client } from "@/sanity/lib/client"
-import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries"
+// import { client } from "@/sanity/lib/client"
+// import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries"
 import { notFound } from "next/navigation"
 import Image from "next/image"
 import UserStartups from "../../../components/UserStartups"
 import { Suspense } from "react"
-import { StartupCardSkeleton } from "../../../components/DeckCard"
+import { getAuthorById } from "@/lib/queries/getAuthorById"
+// import { StartupCardSkeleton } from "../../../components/DeckCard"
 
 export const experimental_ppr = true
 
@@ -13,7 +14,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id
   const session = await auth()
 
-  const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id })
+  const user = await getAuthorById(Number(id))
   if (!user) return notFound()
 
   return (
@@ -22,13 +23,13 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <div className="profile_card">
           <div className="profile_title">
             <h3 className="text-24-black uppercase text-center line-clamp-1">
-              {user.name}
+              {user.username}
             </h3>
           </div>
 
           <Image
             src={user.image}
-            alt={user.name}
+            alt={user.username}
             width={220}
             height={220}
             className="profile_image"
@@ -42,12 +43,12 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
           <p className="text-30-bold">
-            {session?.id === id ? "Your" : "All"} Startups
+            {session?.user?.email === user?.email ? "Your" : "All"} Startups
           </p>
           <ul className="card_grid-sm">
-            <Suspense fallback={<StartupCardSkeleton />}>
+            {/* <Suspense fallback={<StartupCardSkeleton />}>
               <UserStartups id={id} />
-            </Suspense>
+            </Suspense> */}
           </ul>
         </div>
       </section>
